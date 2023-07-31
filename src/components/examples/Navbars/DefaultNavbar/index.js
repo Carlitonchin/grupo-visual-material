658f2425ab988134e8a5173639a895597a34d37b
CarlosArrieta <carrieta@defontana.com>
2023-07-31 17:41:44 -0300

    change navbar color

--- src/components/examples/Navbars/DefaultNavbar/index.js
+++ src/components/examples/Navbars/DefaultNavbar/index.js
@@ -503,7 +503,7 @@ function DefaultNavbar({
         mx={relative ? 0 : 3}
         width={relative ? "100%" : "calc(100% - 48px)"}
         borderRadius="xl"
-        className="bg-gray-700 bg-opacity-80"
+        className="bg-black bg-opacity-60 "
         shadow={transparent ? "none" : "md"}
         color={light ? "white" : "dark"}
         position={relative ? "relative" : "absolute"}
