724116a4f68c03961acf26dbefe3ba794d783d98
CarlosArrieta <carrieta@defontana.com>
2023-08-02 15:19:52 -0300

    adjust star card responsively

--- src/components/cards/StarCard/index.js
+++ src/components/cards/StarCard/index.js
@@ -14,13 +14,13 @@ export default function StarCard({ img, text, stars, url }) {
     <a href={url}>
       <div
         className={
-          "ml-4 mb-4 cursor-pointer overflow-hidden w-[28vw] h-[29vw] min-w-[300px] min-h-[311px] max-w-[30rem] max-h-[31rem] shadow-md hover:shadow-xl duration-300 rounded-md"
+          "ml-4 mb-4 cursor-pointer overflow-hidden w-[28vw] h-[31vw] min-w-[300px] min-h-[311px] max-w-[30rem] max-h-[31rem] shadow-md hover:shadow-xl duration-300 rounded-md"
         }
         style={{ border: "0.2px solid rgba(242, 237, 237, 0.071);" }}
         onMouseEnter={() => setHover(true)}
         onMouseLeave={() => setHover(false)}
       >
-        <div className="w-full h-[65%] overflow-hidden relative">
+        <div className="w-full h-[55%] xl:h-[65%] overflow-hidden relative">
           <div
             className={
               "absolute top-0 left-0 w-full h-full transition-all duration-500 " +
@@ -39,35 +39,37 @@ export default function StarCard({ img, text, stars, url }) {
             }
           />
         </div>
-        <div className="w-full h-[35%] p-4 py-1 flex flex-col items-center justify-center gap-y-4">
-          <MKTypography variant="h4" color="black" fontWeight="bold">
-            {text}
-          </MKTypography>
-          <div className="w-full -mt-3 flex items-center justify-center gap-x-1">
-            {new Array(Math.ceil(stars))
-              .fill(0)
-              .map((_, index) =>
-                index + 1 <= stars ? (
-                  <StarIcon
-                    className="w-6 h-6"
-                    style={{ color: colors.warning.main }}
-                  />
-                ) : (
-                  <></>
-                )
+        <div className="w-full h-[45%] xl:h-[35%] p-4 pt-4 py-2 flex flex-col items-center justify-between gap-y-4">
+          <div className="flex flex-col gap-y-4">
+            <MKTypography variant="h4" color="black" fontWeight="bold">
+              {text}
+            </MKTypography>
+            <div className="w-full -mt-3 flex items-center justify-center gap-x-1">
+              {new Array(Math.ceil(stars))
+                .fill(0)
+                .map((_, index) =>
+                  index + 1 <= stars ? (
+                    <StarIcon
+                      className="w-6 h-6"
+                      style={{ color: colors.warning.main }}
+                    />
+                  ) : (
+                    <></>
+                  )
+                )}
+              {Math.ceil(stars) > stars && (
+                <StarHalfIcon
+                  className="w-6 h-6"
+                  style={{ color: colors.warning.main }}
+                />
               )}
-            {Math.ceil(stars) > stars && (
-              <StarHalfIcon
-                className="w-6 h-6"
-                style={{ color: colors.warning.main }}
-              />
-            )}
-            {new Array(5 - Math.ceil(stars)).fill(0).map(() => (
-              <StarBorderIcon
-                className="w-6 h-6"
-                style={{ color: colors.warning.main }}
-              />
-            ))}
+              {new Array(5 - Math.ceil(stars)).fill(0).map(() => (
+                <StarBorderIcon
+                  className="w-6 h-6"
+                  style={{ color: colors.warning.main }}
+                />
+              ))}
+            </div>
           </div>
           <MKButton
             variant="gradient"
