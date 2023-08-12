3b02dd0156fd74ab43a1a53a00d0f4b38e33aa08
CarlosArrieta <carrieta@defontana.com>
2023-08-12 13:08:13 -0300

    more space between brands

--- src/app/page-components/home/brand.jsx
+++ src/app/page-components/home/brand.jsx
@@ -1,6 +1,23 @@
+"use client";
+import MKTypography from "@/components/MKTypography";
+import colors from "@/theme/base/colors";
+
 export default function Brand() {
   return (
-    <section className="flex justify-center items-center flex-wrap gap-x-8 lg:gap-x-12 gap-y-4">
+    <section className="flex justify-center items-center flex-wrap gap-x-8 md:gap-x-12 lg:gap-x-16 xl:justify-between gap-y-4">
+      <MKTypography
+        variant="h2"
+        color={colors.dark.main}
+        textGradient
+        className="text-center w-full"
+        sx={({ breakpoints, typography: { size } }) => ({
+          [breakpoints.down("md")]: {
+            fontSize: size["4xl"],
+          },
+        })}
+      >
+        Brand Partners
+      </MKTypography>
       <img className="w-auto max-w-md max-h-28 h-auto" src="logo1.png"></img>
       <img className="w-auto max-w-md max-h-28 h-auto" src="logo2.png"></img>
       <img className="w-auto max-w-md max-h-28 h-auto" src="logo3.png"></img>
