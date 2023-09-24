44110de7bdb50decf81a8c5a984ae0cf08576116
CarlosArrieta <carrieta@defontana.com>
2023-09-24 18:13:50 -0300

    delete unused components

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,6 +1,3 @@
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
-import theme from "@/theme";
 import { CartProvider } from "./hooks/cart";
 import { ReactPixelProvider } from "./hooks/reactPixel";
 import { OrderProvider } from "./hooks/order";
