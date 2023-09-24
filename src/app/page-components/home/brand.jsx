f99abdddd72d2c65ee635e3f15ac9931bb729505
CarlosArrieta <carrieta@defontana.com>
2023-09-24 15:13:37 -0300

    brand section without mk

--- src/app/page-components/home/brand.jsx
+++ src/app/page-components/home/brand.jsx
@@ -1,26 +1,10 @@
-"use client";
-import MKTypography from "@/components/MKTypography";
-import colors from "@/theme/base/colors";
-
 export default function Brand({ brands, title }) {
   return (
-    <section className="flex justify-center items-center flex-wrap gap-x-8 md:gap-x-12 lg:gap-x-16 xl:justify-between gap-y-4">
-      <MKTypography
-        variant="h2"
-        color={colors.dark.main}
-        textGradient
-        className="text-center w-full"
-        sx={({ breakpoints, typography: { size } }) => ({
-          [breakpoints.down("md")]: {
-            fontSize: size["4xl"],
-          },
-        })}
-      >
-        {title}
-      </MKTypography>
+    <section className="flex justify-center items-center flex-wrap gap-x-8 md:gap-x-12 gap-y-4">
+      <h2 className="text-center w-full">{title}</h2>
       {brands.map((brand, index) => (
         <img
-          className="w-auto max-w-md max-h-28 h-auto"
+          className="w-auto max-w-md max-h-20 h-auto"
           src={brand.img}
           alt={brand.alt}
           key={index}
